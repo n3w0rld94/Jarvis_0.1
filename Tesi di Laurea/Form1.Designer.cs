e431b312843ff6d5a744f8f975b0a805806a3120
n3w0rld94 <203288@studenti.unimore.it>
2016-09-20 06:59:13 +0200

    Add files via Upload
    The core of the speech recognizer

---
+++ Tesi di Laurea/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Tesi_di_Laurea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_start.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_start.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_start.Location = new System.Drawing.Point(0, 53);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(124, 31);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start Jarvis 0.1";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(0, -1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Apri nuovo Dizionario...";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 513);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(861, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // btn_read
+            // 
+            this.btn_read.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_read.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_read.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_read.FlatAppearance.BorderSize = 0;
+            this.btn_read.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_read.Font = new System.Drawing.Font("Modern No. 20", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_read.Location = new System.Drawing.Point(0, 130);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(80, 31);
+            this.btn_read.TabIndex = 4;
+            this.btn_read.Text = "Lecture";
+            this.btn_read.UseVisualStyleBackColor = false;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_stop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_stop.Enabled = false;
+            this.btn_stop.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_stop.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_stop.Location = new System.Drawing.Point(0, 93);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(98, 31);
+            this.btn_stop.TabIndex = 5;
+            this.btn_stop.Text = "Stop Jarvis";
+            this.btn_stop.UseVisualStyleBackColor = false;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.HideSelection = false;
+            this.TextBox.Location = new System.Drawing.Point(143, 53);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(420, 108);
+            this.TextBox.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "*.txt";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(612, 185);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_enable;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem generaRispostaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem standardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dynamicResponseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem experimentalToolStripMenuItem;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Tesi di Laurea/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+namespace Tesi_di_Laurea
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer Jarvis_talk = new SpeechSynthesizer();
+        PromptBuilder pbuilder = new PromptBuilder();
+        SpeechRecognitionEngine Jarvis_read = new SpeechRecognitionEngine();
+        string name = "User";
+        int n=0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            //Jarvis_talk.Speak("Greetings Mister" + name + ", my name is Jarvis and my goal is to satisfy you in whichever way you desire. To stop the recognition, say Goodby Jarvis. Now, let's start by loading a Dictionary, and then tell me your name");
+        }
+
+        string[] Dictionary = null;
+        Choices lBox = new Choices();
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            if (Dictionary != null)
+            {
+                lBox.Add(Dictionary);
+                Jarvis_read.SpeechRecognized += Jarvis_read_SpeechRecognized;
+                btn_start.Enabled = false;
+                btn_stop.Enabled = true;
+                Grammar grammar = new Grammar(new GrammarBuilder(lBox));
+                try
+                {
+                    Jarvis_read.RequestRecognizerUpdate();
+                    Jarvis_read.LoadGrammarAsync(grammar);
+
+                    Jarvis_read.SetInputToDefaultAudioDevice();
+                    Jarvis_read.RecognizeAsync(RecognizeMode.Multiple);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "ERRORE");
+                    return;
+                }
+            }
+            else
+                Jarvis_talk.Speak("You must select a dictionary before starting recognition");
+        } 
+
+        private void Jarvis_read_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text.ToString()) {
+                case "How are you":
+                    Jarvis_read.RecognizeAsyncStop();
+                    Jarvis_talk.Speak("Better than good! Thanks");
+                    Jarvis_read.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+                case "Hi Jarvis":
+                    Jarvis_read.RecognizeAsyncStop();
+                    Jarvis_talk.Speak("Yes sir " + name);
+                    Jarvis_read.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+                case "Find oreos for me":
+                    Jarvis_read.RecognizeAsyncStop();
+                    Jarvis_talk.Speak("Maybe you should look for them in the kitchen " + name + " oh lord of the galaxy");
+                    Jarvis_read.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+                case "Goodbye Jarvis":
+                    Jarvis_read.RecognizeAsyncStop();
+                    Jarvis_talk.Speak("Goodbye master " + name);
+                    btn_start.Enabled = true;
+                    btn_stop.Enabled = false;
+                    name = "user";
+                    break;
+                case "Open Opera":
+                    Jarvis_read.RecognizeAsyncStop();
+                    Jarvis_talk.Speak("Done master");
+                    Jarvis_read.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+                case "Tell me a story":
+                    string b;
+                    StreamReader F = File.OpenText("C:/Users/Shea/Documents/visual studio 2015/Projects/Tesi di Laurea/Tesi di Laurea/Stories.txt");
+                    if (F == null)
+                        Jarvis_talk.Speak("I have no story to tell you");
+                    else {
+                        b = F.ReadLine();
+                        Jarvis_talk.Speak("Sure master " + name + b);
+                        b = F.ReadLine();
+                        while ((b != "\n") && (b != null)) {
+                            b = F.ReadLine();
+                            Jarvis_read.RecognizeAsyncStop();
+                            Jarvis_talk.Speak(b);
+                            Jarvis_read.RecognizeAsync(RecognizeMode.Multiple);
+                        }
+                    }
+                    break;
+                case "Ilyas":
+                    name = e.Result.Text.ToString();
+                    Jarvis_read.RecognizeAsyncStop();
+                    Jarvis_talk.Speak("Welcome back " + name);
+                    Jarvis_read.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+                case "Morad":
+                    name = e.Result.Text.ToString();
+                    Jarvis_read.RecognizeAsyncStop();
+                    Jarvis_talk.Speak("Welcome back " + name);
+                    Jarvis_read.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+
+            }
+            TextBox.AppendText(e.Result.Text.ToString() + " ");
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            pbuilder.ClearContent();
+            pbuilder.AppendText(TextBox.Text);
+            Jarvis_talk.Speak(pbuilder);
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            Jarvis_read.RecognizeAsyncStop();
+            btn_start.Enabled = true;
+            btn_stop.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Dictionary = File.ReadAllLines(openFileDialog1.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Errore");
+                }
+            }
+            
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
