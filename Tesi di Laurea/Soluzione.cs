53bf84cc8cb9a955eff4dd621ce3abbad211d3e6
Jymmy097 <Jymmy097@users.noreply.github.com>
2016-10-01 00:31:02 +0200

    Update Soluzione.cs

--- Tesi di Laurea/Soluzione.cs
+++ Tesi di Laurea/Soluzione.cs
@@ -9,9 +9,13 @@ public class FFANN{
 
 public class Layer{
 	public FFANN Parent;
+	private Perceptron[] perceptrons;
 	
 	public Layer(FFANN parent){
 		Parent = parent;
+	}
+	public void AddPerce(int n){
+		perceptrons[n] = new Perceptron(this,...);
 	}
 }
 
